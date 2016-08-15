b243ac2cf5b6705da5e8a3decbdc457ced57e50b
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-08-15 12:53:07 -0700

    Added jsdoc comments to Messages.d.ts

--- Messages.d.ts
+++ Messages.d.ts
@@ -1,25 +1,49 @@
+/**
+ * Valid calculation types.
+ */
 declare enum CalcTypes {
+    /** SRMP to ARM */
     SrmpToArm = 0,
+    /** ARM to SRMP */
     ArmToSrmp = 1
 }
 
-// type ABIndicator = "A" | "B" | "";
+// type ABIndicator = "A" | "B";
 
+/**
+ * Defines an ARM Calc operation.
+ */
 interface ArmCalcInput {
+    /** Calculation Type. 0 = SRMP to ARM, 1 = ARM to SRMP */
     CalcType?: CalcTypes;
+    /** Three digit state route ID. */
     SR: string;
+    /** Related Route Type */
     RRT?: string;
+    /** Related Route Qualifier */
     RRQ?: string;
+    /** Ahead / Back indicator for SRMP. "A" or null, or "B" */
     ABIndicator?: string;
+    /** Input data collection date */
     ReferenceDate: Date;
+    /** Accumulated Route Mileage. Actual measure */
     ARM?: number;
+    /** State Route Milepost - Posted milepost. May not match actual measure due to route adjustments over time. */
     SRMP?: number;
+    /** Output date. Use this to match an LRS publication date. */
     ResponseDate: Date;
+    /** Transaction ID. Use a unique ID with batch results. */
     TransId?: string;
 }
 
+/**
+ * ArmCalc output.
+ */
 interface ArmCalcOutput extends ArmCalcInput {
+    /** Calculation return code. */
     CalculationReturnCode: number;
+    /** Calulation return message. Will not have value if return code is 0. */
     CalculationReturnMessage: string;
+    /** Realignment date. Date that a route was last realigned. */
     RealignmentDate: Date;
 }
